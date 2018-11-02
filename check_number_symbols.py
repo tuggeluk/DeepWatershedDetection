ded0ed3249d2540bdf7a0007c9300a42d77c1df5
root <ismail.elezi@gmail.com>
2018-11-02 12:09:41 +0000

    Generalize the file for all musical datasets we use

--- check_number_symbols.py
+++ check_number_symbols.py
@@ -5,11 +5,10 @@ import pandas as pa
 import sys
 
 
-def count_symbols_muscima(classes, set_file):
+def count_symbols_muscima(classes, set_file, xml_annotations_path):
     # create and initialize the dictionary
     num_syms_per_class = {}
     for k in classes: num_syms_per_class[k] = 0
-    xml_annotations_path = '/DeepWatershedDetection/data/MUSICMA++_2017/MUSICMA++_2017/xml_annotations'
     with open(set_file) as f:
         content = f.readlines()
     content = [x.strip() for x in content]
@@ -24,11 +23,10 @@ def count_symbols_muscima(classes, set_file):
     return num_syms_per_class
 
 
-def count_symbols(classes, set_file):
+def count_symbols(classes, set_file, xml_annotations_path):
     # create and initialize the dictionary
     num_syms_per_class = {}
     for k in classes: num_syms_per_class[k] = 0
-    xml_annotations_path = '/DeepWatershedDetection/data/MUSICMA++_2017/MUSICMA++_2017/xml_annotations'
     with open(set_file) as f:
         content = f.readlines()
     content = [x.strip() for x in content]
@@ -41,8 +39,11 @@ def count_symbols(classes, set_file):
     return num_syms_per_class
 
 
-def count_zeros(classes, class_to_ind, dataset):
-    num_syms_per_class = count_symbols_muscima(classes, dataset)
+def count_zeros(classes, class_to_ind, set_file, dataset, xml_annotations_path):
+    if dataset == 'Muscima':
+        num_syms_per_class = count_symbols_muscima(classes, set_file, xml_annotations_path)
+    else:
+        num_syms_per_class = count_symbols(classes, set_file, xml_annotations_path)
     zeros = []
     for k in classes:
         if num_syms_per_class[k] == 0:
@@ -57,28 +58,46 @@ def count(classes, num_syms_per_class):
 
 
 def main():
-    classes = list(pa.read_csv(
-        "/DeepWatershedDetection/data/MUSICMA++_2017/MUSICMA_classification/class_names.csv", header=None)[1])
-    classes_lower = []
-    for k in classes:
-        if 'letter' not in k:
-            kk = k.lower()
-            classes_lower.append(kk)
-        else:
-            classes_lower.append(k)
-    class_to_ind = dict(list(zip(classes_lower, list(range(len(classes))))))
-    #class_to_ind = dict(list(zip(classes, list(range(len(classes))))))
-    # print(class_to_ind)
-    train_set_file = '/DeepWatershedDetection/data/MUSICMA++_2017/train_val_test/train.txt'
-    test_set_file = '/DeepWatershedDetection/data/MUSICMA++_2017/train_val_test/test.txt'
+    dataset = 'Muscima'
+    if dataset == 'Muscima':
+        classes = list(pa.read_csv(
+            "/DeepWatershedDetection/data/MUSICMA++_2017/MUSICMA_classification/class_names.csv", header=None)[1])
+        classes_lower = []
+        for k in classes:
+            if 'letter' not in k:
+                kk = k.lower()
+                classes_lower.append(kk)
+            else:
+                classes_lower.append(k)
+        class_to_ind = dict(list(zip(classes_lower, list(range(len(classes))))))
+        train_set_file = '/DeepWatershedDetection/data/MUSICMA++_2017/train_val_test/train.txt'
+        test_set_file = '/DeepWatershedDetection/data/MUSICMA++_2017/train_val_test/test.txt'
+        xml_annotations_path = '/DeepWatershedDetection/data/MUSICMA++_2017/MUSICMA++_2017/xml_annotations'
+        zeros_test, num_syms_per_class_test = count_zeros(classes_lower, class_to_ind, test_set_file, dataset, xml_annotations_path)
+        zeros_train, num_syms_per_class_train = count_zeros(classes_lower, class_to_ind, train_set_file, dataset, xml_annotations_path)
+    else:
+        if dataset == 'DeepScores':
+            classes = list(pa.read_csv(
+                "/DeepWatershedDetection/data/DeepScores_2017/DeepScores_classification/class_names.csv", header=None)[1])
+            train_set_file = '/DeepWatershedDetection/data/DeepScores_2017/train_val_test/train.txt'
+            test_set_file = '/DeepWatershedDetection/data/DeepScores_2017/train_val_test/test.txt'
+            xml_annotations_path = '/DeepWatershedDetection/data/DeepScores_2017/segmentation_detection/xml_annotations'
 
-    zeros_test, num_syms_per_class_test = count_zeros(classes_lower, class_to_ind, test_set_file)
-    zeros_train, num_syms_per_class_train = count_zeros(classes_lower, class_to_ind, train_set_file)
+        elif dataset == 'DeepScores_300dpi':
+            classes = list(pa.read_csv(
+                "/DeepWatershedDetection/data/DeepScores_3000dpi_2017/DeepScores_classification/class_names.csv", header=None)[1])
+            train_set_file = '/DeepWatershedDetection/data/DeepScores_300dpi_2017/train_val_test/train.txt'
+            test_set_file = '/DeepWatershedDetection/data/DeepScores_300dpi_2017/train_val_test/test.txt'
+            xml_annotations_path = '/DeepWatershedDetection/data/DeepScores_300dpi_2017/segmentation_detection/xml_annotations'
+
+        class_to_ind = dict(list(zip(classes, list(range(len(classes))))))
+        zeros_test, num_syms_per_class_test = count_zeros(classes, class_to_ind, test_set_file, dataset, xml_annotations_path)
+        zeros_train, num_syms_per_class_train = count_zeros(classes, class_to_ind, train_set_file, dataset, xml_annotations_path)
+
+
     num_syms_per_class_total = {}
     for k in num_syms_per_class_test:
         num_syms_per_class_total[k] = num_syms_per_class_test[k] + num_syms_per_class_train[k]
-    #zeros_test, num_syms_per_class_test = count_zeros(classes, class_to_ind, test_set_file)
-    #zeros_train, num_syms_per_class_train = count_zeros(classes, class_to_ind, train_set_file)
 
     print(num_syms_per_class_test)
     print("\n\n\n\n\n")
